0c24e067036169f5120b039936a176afd018a7c1
Jorge Henrique Dos Santos Ferraz <jferraz@cpqd.com.br>
2020-02-17 15:28:17 -0300

    Projeto Spring Boot criado

---
+++ src/main/java/com/projetocurso/springboot/ProjetocursospringbootApplication.java
@@ -0,0 +1,13 @@
+package com.projetocurso.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetocursospringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetocursospringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/com/projetocurso/springboot/ProjetocursospringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.com.projetocurso.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetocursospringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
