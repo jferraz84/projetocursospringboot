d8d631f05a92cb83d733cbc59aab6dc9a890c84e
Jorge Henrique Dos Santos Ferraz <jferraz@cpqd.com.br>
2020-02-21 14:38:07 -0300

    Associação muitos-para-muitos com JoinTable

--- src/main/java/com/projetocurso/springboot/config/TestConfig.java
+++ src/main/java/com/projetocurso/springboot/config/TestConfig.java
@@ -47,6 +47,18 @@ public class TestConfig implements CommandLineRunner {
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p4, p5));
+		
 		User u1 = new User (null, "Aline", "aferraz@cpqd.com", "90102-0304", "123456");
 		User u2 = new User (null, "Helo", "heloferraz@cpqd.com", "90201-0403", "654321");
 		
@@ -55,10 +67,10 @@ public class TestConfig implements CommandLineRunner {
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WATTIING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WATTIING_PAYMENT, u1);
 		
-		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
-		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		
 	}
 	
 	
