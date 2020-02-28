19e0836ca3f375b8bff82f57ce6ea277cebe54f7
Jorge Henrique Dos Santos Ferraz <jferraz@cpqd.com.br>
2020-02-28 14:17:30 -0300

    Tratamento de exceção - delete

--- src/main/java/com/projetocurso/springboot/services/UserService.java
+++ src/main/java/com/projetocurso/springboot/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.projetocurso.springboot.entities.User;
 import com.projetocurso.springboot.repositories.UserRepository;
+import com.projetocurso.springboot.services.exceptions.DatabaseException;
 import com.projetocurso.springboot.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -31,7 +34,14 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
